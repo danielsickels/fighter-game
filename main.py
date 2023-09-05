e4c37c99bcd91791fc5fb8e9578c0cdb87543a7a
danielsickels <drsickels@gmail.com>
2023-09-05 17:53:23 -0400

    created fighter names, attack method, starting new methods

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random
 
 class Fighter():
-    def __init__(self, name, health = 50, defense = 5, strength = 5, speed = 5, intellect = 5):
+    def __init__(self, name, health = 20, defense = 5, strength = 5, speed = 5, intellect = 5):
         self.name = name
         self.health = health
         self.defense = defense
         self.strength = strength
         self.speed = speed
         self.intellect = intellect
-    
+        self.valid_actions = ["Attack", "Defend", "Boost Speed"]
+
     def __repr__(self):
         return self.name
     
@@ -18,11 +19,31 @@ class Fighter():
         print(f"Strength: {self.strength}")
         print(f"Speed: {self.speed}")
         print(f"Intellect: {self.intellect}")
+    
+    def attack(self, other_player):
+        damage = self.strength - other_player.defense
+        if damage <= 0:
+            damage = 0
+        other_player.health -= damage
+        return f"{self.name} did {damage} damage to {other_player.name}!"
+        
 
-big_d = Fighter("Danny")
-lil_d = Fighter("Mike", speed = 6)
+    def defend(self):
+        self.defense += 1
+        self.speed += 1
+    
+    def boost_speed(self):
+        self.speed += 2    
+    
+
+big_d = Fighter("Danny", strength= 8)
+lil_d = Fighter("Mike")
 
 current_attacker = big_d
 
 if big_d.speed < lil_d.speed:
     current_attacker = lil_d
+
+print(big_d.attack(lil_d))
+print(big_d.attack(lil_d))
+print(f"{lil_d.name} has {lil_d.health} health.")
